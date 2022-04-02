12f5fa6c6869102d7ad6502548048b8e26ea55e4
Bolotov Alexander <distalker1@gmail.com>
2022-04-02 09:03:46 +0300

    Initial commit

---
+++ src/main/java/ru/kata/money_tracker_service/MoneyTrackerServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.kata.money_tracker_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTrackerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyTrackerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kata/money_tracker_service/MoneyTrackerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kata.money_tracker_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTrackerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
