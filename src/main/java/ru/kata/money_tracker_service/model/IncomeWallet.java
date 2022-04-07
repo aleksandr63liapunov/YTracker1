cfdf56ad854499e003e89b053a3d4d1661575b2c
aleks63liapunov <rodrlgo63@mail.ru>
2022-04-07 18:24:06 +0400

    change 10

--- src/main/java/ru/kata/money_tracker_service/model/IncomeWallet.java
+++ src/main/java/ru/kata/money_tracker_service/model/IncomeWallet.java
@@ -1,6 +1,5 @@
 package ru.kata.money_tracker_service.model;
 
-<<<<<<< HEAD
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -17,18 +16,8 @@ public class IncomeWallet {
     @Id
     @Setter(value = AccessLevel.NONE)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-=======
-import javax.persistence.*;
-
-@Entity
-public class IncomeWallet {
-    @Id
-    @Column(name = "id", nullable = false)
->>>>>>> b3a2af47c613e0f77e54eb0712061eb15056d6a2
     private Long id;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Transaction transaction;
-
-
 }
